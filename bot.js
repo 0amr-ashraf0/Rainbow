d92909d340bcb089f828614406572cdcbf10886c
0amr-ashraf0 <46089737+0amr-ashraf0@users.noreply.github.com>
2018-12-27 10:43:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ if(!message.channel.guild) return;
   if(!role){
     rainbow =  message.guild.createRole({
    name: "Rainbow ",//اسم الرتبه 
-   color: "#000000",//الون الاساسي للرنبو 
+   color: "RANDOM",//الون الاساسي للرنبو 
    permissions:[]//الرتبه المسموح بيها للرنبو  مثال MANAGE_ROLES ADMINISTRATOR   
  //نهايه الكود هنا
 })
